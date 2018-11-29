4c4a8b428513b11e12ab455fb1b4a830869c5600
Tim Cambrant <cambrant@gmail.com>
2018-11-29 12:05:53 +0100

    Finished day 5.

---
+++ day5_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+lines = []
+
+with open('day5_input.txt', 'r') as f:
+    for line in f.readlines():
+        lines.append(int(line.strip()))
+
+current_address = 0
+number_of_instructions = len(lines)
+
+step_count = 0
+try:
+    while True:
+        next_address = current_address + lines[current_address]
+        if lines[current_address] >= 3:
+            lines[current_address] -= 1
+        else:
+            lines[current_address] += 1
+        current_address = next_address
+        step_count += 1
+except:
+    print step_count
